e9037b960f57d7e6c1a0b1de8af2fc4175ccfe5a
Filipsi <Filip.sikula@seznam.cz>
2017-05-22 22:00:40 +0200

    Crude draft

---
+++ src/GuardianRP.Api/GuardianRP.Api/Api.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuardianRP.Api {
+
+    class Api {
+
+        public static readonly TcpServer Server = new TcpServer(25567);
+
+        static void Main(string[] args) {
+            Server.Start();
+            
+            while(true) {
+                Server.Broadcast(Console.ReadLine());
+            }
+        }
+
+    }
+
+}
---
+++ src/GuardianRP.Api/GuardianRP.Api/ClientHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuardianRP.Api.Server {
+
+    internal class ClientHandler {
+
+        public readonly Socket Socket;
+
+        public ClientHandler(Socket client) {
+            Socket = client;
+        }
+
+    }
+
+}
